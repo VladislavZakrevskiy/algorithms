35cd0c80b8140cecfeaadb0a5f64f38dabd272eb
Zakrevskiy Vladislav <zakrevskiyvlada@yandex.ru>
2025-06-28 09:21:32 +0300

    added sorting

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,26 @@ import GA from './pages/GA'
 import Ant_alg from './pages/Ant_alg'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import { Sorting } from './pages/Sorting'
 
 function App() {
-	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Nav />} />
-					<Route path="/gravity" element={<Gravity />} />
-					<Route path="/genetic_algorithm" element={<GA />} />
-					<Route path="/raycasting" element={<Raycasting />} />
-					<Route path="/cullular_automat_3d" element={<Cullular_automat />} />
-					<Route path="/roy_algorithm" element={<Roy_algorithm />} />
-					<Route path="/ant_algorithm" element={<Ant_alg />} />
-				</Routes>
-			</BrowserRouter>
-		</Provider>
-	)
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Nav />} />
+                    <Route path="/gravity" element={<Gravity />} />
+                    <Route path="/genetic_algorithm" element={<GA />} />
+                    <Route path="/raycasting" element={<Raycasting />} />
+                    <Route path="/cullular_automat_3d" element={<Cullular_automat />} />
+                    <Route path="/roy_algorithm" element={<Roy_algorithm />} />
+                    <Route path="/ant_algorithm" element={<Ant_alg />} />
+                    <Route path="/sort" element={<Sorting />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    )
 }
 
 export default App
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,29 @@
-import { Box } from "@mui/material"
-import NavButton from './NavButton';
-import { Home } from "@mui/icons-material";
-import { Link } from 'react-router-dom';
+import { Box } from '@mui/material'
+import NavButton from './NavButton'
+import { Home } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <Box sx={{
-            m: 2,
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            gap: 3
-        }}>
-            <Link to={'/'} style={{fontSize: "64px"}}><Home fontSize="inherit" color="primary"/></Link>
+        <Box
+            sx={{
+                m: 2,
+                display: 'flex',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                gap: 3,
+            }}
+        >
+            <Link to={'/'} style={{ fontSize: '64px' }}>
+                <Home fontSize="inherit" color="primary" />
+            </Link>
             <NavButton to={'/gravity'}>Gravity</NavButton>
             <NavButton to={'/genetic_algorithm'}>Genetic algorithm</NavButton>
             <NavButton to={'/raycasting'}>Raycasting</NavButton>
             <NavButton to={'/cullular_automat_3d'}>Cullular auto 3D</NavButton>
             <NavButton to={'/roy_algorithm'}>Roy Algorithm</NavButton>
             <NavButton to={'/ant_algorithm'}>Ant Algorithm</NavButton>
+            <NavButton to={'/sort'}>Sorting</NavButton>
         </Box>
     )
 }
